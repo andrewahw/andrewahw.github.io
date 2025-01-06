939cf67bd460ff6499af576f256288dcf8e9df48
Andrew <64256177+andrewahw@users.noreply.github.com>
2025-01-06 19:08:26 +0000

    trying putting button in utils

--- scripts/signalcomposition.js
+++ scripts/signalcomposition.js
@@ -2,11 +2,11 @@ const canvas = document.getElementById("simulationCanvas");
 const ctx = canvas.getContext("2d");
 const simulationDiv = document.getElementById("simulation");
 
-
 canvas.width = simulationDiv.clientWidth;
 canvas.height = simulationDiv.clientHeight;
 
-const cornerRadius = 15;
+import { cornerRadius, button } from "./utils";
+
 var mousePos = [-1,-1]; //-1 -1 is when mouse is not over the simulation
 var mouseStartPos = [-1,-1]; // updates when mouse pressed, resets when mouse released
 var mouseDown = false;
@@ -17,82 +17,6 @@ function mMove(event) {mousePos = [event.offsetX, event.offsetY];}
 function mExit() {mousePos = [-1, -1]; mouseDown = false; prevMouseDown = false;}
 function mDown() {mouseDown = true;}
 function mUp() {mouseDown = false;}
-
-function button(position, dimensions, imgLink, imgDimensions, colour, colourTransition, borderRadius, transition, transitionScale,onClick, onClickArguments) {
-
-    this.origPos = position;
-    this.origDim = dimensions;
-    this.origBorderRadius = borderRadius;
-    this.origImgDim = imgDimensions;
-
-    this.pos = position; //position and dimensions are a 2d vector with x/width and y/height respectively
-    this.dim = dimensions;
-    this.onClick = function() {onClick(onClickArguments);}
-
-    this.imageLink = "../images/" + imgLink
-    this.image = new Image();
-    this.image.src = this.imageLink
-    this.imageDim = imgDimensions;
-
-    this.origColour = colour; // colours are in 4d array (rgba) to make colour interpolation easier
-    this.transColour = colourTransition; // colour it transitions to when hovered
-    this.colour = colour; // current colour
-    this.borderRadius = borderRadius;
-
-    this.currentTransition = 0;
-    this.maxTransition = transition;
-    this.transitionMaxScale = transitionScale;
-
-    this.buttonLoop = function() {
-        
-        //Mouse user handling
-        if(mousePos[0] > this.pos[0] && mousePos[0] < (this.pos[0] + this.dim[0])
-        && mousePos[1] > this.pos[1] && mousePos[1] < (this.pos[1] + this.dim[1])) {
-            this.currentTransition = Math.min(this.currentTransition + 1, this.maxTransition);
-            if(mouseDown == true && prevMouseDown == false) {this.onClick();}
-        }
-        else {
-            this.currentTransition = Math.max(this.currentTransition - 1, 0);
-        }
-
-        //Position and dimension and colour updating
-        var percentTransition = this.currentTransition / this.maxTransition;
-        var currentAnimation = (2 / (1 + Math.exp(-6 * percentTransition)));
-        var currentScale = 1 + ((transitionScale - 1) * currentAnimation);
-
-        this.dim = [this.origDim[0] * currentScale, this.origDim[1] * currentScale];
-        this.imageDim = [this.origImgDim[0] * currentScale, this.origImgDim[1] * currentScale];
-        this.pos = [this.origPos[0] - (0.5 * this.dim[0]), this.origPos[1] - (0.5 * this.dim[1])];
-
-        this.borderRadius = this.origBorderRadius * ((1.2 / (1 + Math.exp(6 * percentTransition))) + 0.4); // border is special
-
-        //note: I know there's better ways of colour interpolation, but in general the animations are pretty short so shouldn't matter
-        var currentColourArray = [
-            this.origColour[0] + ((this.transColour[0] - this.origColour[0]) * percentTransition),
-            this.origColour[1] + ((this.transColour[1] - this.origColour[1]) * percentTransition),
-            this.origColour[2] + ((this.transColour[2] - this.origColour[2]) * percentTransition),
-            this.origColour[3] + ((this.transColour[3] - this.origColour[3]) * percentTransition),
-        ]
-        this.colour = "#"
-            + Math.floor(currentColourArray[0]).toString(16)
-            + Math.floor(currentColourArray[1]).toString(16)
-            + Math.floor(currentColourArray[2]).toString(16)
-            + Math.floor(currentColourArray[3] * 255).toString(16)
-    }
-
-    this.buttonDraw = function() {
-        ctx.beginPath(); //Main body
-        ctx.fillStyle = this.colour;
-        ctx.arc(this.pos[0] + this.borderRadius, this.pos[1] + this.borderRadius, this.borderRadius, Math.PI, 1.5 * Math.PI);
-        ctx.arc(this.pos[0] + this.dim[0] - this.borderRadius, this.pos[1] + this.borderRadius, this.borderRadius, 1.5 * Math.PI, 0);
-        ctx.arc(this.pos[0] + this.dim[0] - this.borderRadius, this.pos[1] + this.dim[1] - this.borderRadius, this.borderRadius, 0, 0.5 * Math.PI);
-        ctx.arc(this.pos[0] + this.borderRadius, this.pos[1] + this.dim[1] - this.borderRadius, this.borderRadius, 0.5 * Math.PI, Math.PI);
-        ctx.fill();
-
-        ctx.drawImage(this.image,this.pos[0] + ((this.dim[0] - this.imageDim[0]) / 2),
-            this.pos[1] + ((this.dim[1] - this.imageDim[1]) / 2),this.imageDim[0],this.imageDim[1]);
-    }
-}
 
 //Buttons!
 const buttonList = [
