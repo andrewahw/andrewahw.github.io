8b621ff8bd8d663bda4ea71d30ceda043e3956a3
Andrew <64256177+andrewahw@users.noreply.github.com>
2025-01-07 18:58:09 +0000

    man am i silly

--- scripts/imagecompression.js
+++ scripts/imagecompression.js
@@ -7,7 +7,7 @@ const simulationDiv = document.getElementById("simulation");
 canvas.width = simulationDiv.clientWidth;
 canvas.height = simulationDiv.clientHeight;
 
-import { cornerRadius, button, slider, drawBackground, FFT, inverseFFT} from "./utils.js";
+import { cornerRadius, button, slider, drawBackground, complex, FFT, inverseFFT} from "./utils.js";
 
 var mousePos = [-1,-1]; //-1 -1 is when mouse is not over the simulation
 var mouseStartPos = [-1,-1]; // updates when mouse pressed, resets when mouse released
