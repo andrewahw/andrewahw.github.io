ceba797130b5dc97ac33d031dafeaf038652ffcd
Andrew <64256177+andrewahw@users.noreply.github.com>
2025-02-01 14:30:49 +0000

    grr

--- scripts/epicycletracing.js
+++ scripts/epicycletracing.js
@@ -123,7 +123,7 @@ function mainLoop() {
             for(var i = 0; i < newSampleLen; i++) {
                 epicycles.push(new epicycle(frequencies[i].mod / newSampleLen, i, frequencies[i].arg));
                 if(i > newSampleLen / 2)
-                    {epicycles[i - 1].angularVelocity = -1 * (i - (newSampleLen / 2))}
+                    {epicycles[i - 1].angularVelocity = -newSampleLen + i}
             }
             epicycles.sort((a, b) => b.radius - a.radius) // sort epicycles by radius
             console.log(samples)
