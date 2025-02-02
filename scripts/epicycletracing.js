cb45698927c9c072c9d26f37aa25ebbd956d0d1b
Andrew <64256177+andrewahw@users.noreply.github.com>
2025-02-02 23:41:29 +0000

    trying things aesthetcially

--- scripts/epicycletracing.js
+++ scripts/epicycletracing.js
@@ -161,8 +161,8 @@ function mainLoop() {
 
             //Draw disc around epicycle
             if(epicycles[i].radius > 5) {
-                ctx.lineWidth = 8 * (epicycles[i].radius / epicycles[1].radius);
-                ctx.strokeStyle = "rgba(255, 194, 12, 0.6)"
+                ctx.lineWidth = (6 * (epicycles[i].radius / epicycles[1].radius)) + 2;
+                ctx.strokeStyle = "rgba(255, 194, 12, 0.4)"
                 ctx.beginPath(); 
                 ctx.arc(currentPos[0],currentPos[1],epicycles[i].radius,0,Math.PI * 2);
                 ctx.stroke();
